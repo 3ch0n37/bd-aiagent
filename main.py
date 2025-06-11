e6c16140e625b70b9dfc2b76266d88c4e1368a95
Vladimir Rujević <vladimir.rujevic@live.com>
2025-06-11 22:16:47 +0200

    Use Google AI's message list instead of a string as a prompt
    Establishes the way to provide message history and context to the AI
    for future prompts.

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 if len(sys.argv) < 2:
     print("Usage: main.py <prompt>")
     sys.exit(1)
 
-prompt = sys.argv[1]
+user_prompt = sys.argv[1]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages
 )
 print(response.text)
 
