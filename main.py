7d8df4cf72f452a226e71d807b7263b913e7cc6c
Vladimir Rujević <vladimir.rujevic@live.com>
2025-06-25 18:54:34 +0200

    I'M JUST A ROBOT!

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+#! /usr/bin/env python3
+
 import os
 import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
 def main():
     load_dotenv()
@@ -33,7 +37,8 @@ def main():
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
     )
     if verbose:
         print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
