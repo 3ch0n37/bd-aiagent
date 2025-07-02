d54aa373700e01d9b91103d21ed7531c19b8a0ef
Vladimir Rujević <vladimir.rujevic@live.com>
2025-07-02 17:49:04 +0200

    Add a call_function function for the AI agent to use
    - actually call the function instead of just printing the function call

--- call_function.py
+++ call_function.py
@@ -1,9 +1,9 @@
 from google.genai import types
 
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_python_file import schema_run_python_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.write_file import schema_write_file, write_file
+from functions.run_python_file import schema_run_python_file, run_python_file
 
 available_functions = types.Tool(
     function_declarations=[
@@ -13,3 +13,43 @@ available_functions = types.Tool(
         schema_write_file,
     ]
 )
+
+function_map = {
+    'get_files_info': get_files_info,
+    'get_file_content': get_file_content,
+    'write_file': write_file,
+    'run_python_file': run_python_file
+}
+
+def call_function(function_call_part: types.FunctionCall, verbose: bool = False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+
+    arguments = function_call_part.args
+    arguments['working_directory'] = './calculator'
+
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    function_result = function_map[function_name](**arguments)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 def main():
@@ -54,7 +54,7 @@ def generate_content(client, messages, verbose):
         print(response.text)
 
     for function_call_part in response.function_calls:
-        print(f'Calling function: {function_call_part.name}({function_call_part.args})')
+        call_function(function_call_part, verbose)
 
 
 if __name__ == '__main__':
